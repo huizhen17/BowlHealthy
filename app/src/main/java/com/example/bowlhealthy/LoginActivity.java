237487fe19711c2ae642313177494cbde1376441
huizhen17 <63848933+huizhen17@users.noreply.github.com>
2020-09-23 17:13:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bowlhealthy/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.bowlhealthy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void linkOnClick_signIn(View view) {
+    }
+
+    public void btnOnClick_Login(View view) {
+    }
+}
