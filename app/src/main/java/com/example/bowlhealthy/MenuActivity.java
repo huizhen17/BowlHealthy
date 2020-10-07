28dd25d26b075ed79fe33e6dcd4fac74fdf7e28a
huizhen17 <63848933+huizhen17@users.noreply.github.com>
2020-10-07 20:55:39 +0800

    Done menu

--- app/src/main/java/com/example/bowlhealthy/MenuActivity.java
+++ app/src/main/java/com/example/bowlhealthy/MenuActivity.java
@@ -17,7 +17,7 @@ public class MenuActivity extends AppCompatActivity {
     String type;
     RecyclerView mRecyclerView;
     MenuAdapter menuAdapter;
-    ArrayList<MenuDetail> pokeList, buddhaList, burritoList, smoothieList ;
+    ArrayList<MenuDetail> menuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +36,40 @@ public class MenuActivity extends AppCompatActivity {
         type = bundle.getString("title");//base on the title get thn if(title.equal("Poke Bowl")) show list
         mMenuType.setText(type);
 
-        //Swipe view for home menu category
-        pokeList = new ArrayList<>();
-        pokeList.add(new MenuDetail(R.drawable.poke1,R.string.poke1Name,R.string.poke1Ing, "13.50","20"));
-        pokeList.add(new MenuDetail(R.drawable.poke2,R.string.poke2Name,R.string.poke2Ing, "13.50","20"));
-        pokeList.add(new MenuDetail(R.drawable.poke3,R.string.poke3Name,R.string.poke3Ing, "12.50","15"));
-        pokeList.add(new MenuDetail(R.drawable.poke4,R.string.poke4Name,R.string.poke4Ing, "12.50","15"));
-        pokeList.add(new MenuDetail(R.drawable.poke5,R.string.poke5Name,R.string.poke5Ing, "12.50","15"));
+        menuList = new ArrayList<>();
+
+        if(type.equalsIgnoreCase("Poke Bowl")){
+            //Swipe view for home menu category
+            menuList.add(new MenuDetail(R.drawable.poke1,R.string.poke1Name,R.string.poke1Ing, "13.50","20"));
+            menuList.add(new MenuDetail(R.drawable.poke2,R.string.poke2Name,R.string.poke2Ing, "13.50","20"));
+            menuList.add(new MenuDetail(R.drawable.poke3,R.string.poke3Name,R.string.poke3Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.poke4,R.string.poke4Name,R.string.poke4Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.poke5,R.string.poke5Name,R.string.poke5Ing, "12.50","15"));
+        }
+        else if(type.equalsIgnoreCase("Buddha Bowl")){
+            menuList.add(new MenuDetail(R.drawable.buddha1,R.string.buddha1Name,R.string.buddha1Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.buddha2,R.string.buddha2Name,R.string.buddha2Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.buddha3,R.string.buddha3Name,R.string.buddha3Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.buddha4,R.string.buddha4Name,R.string.buddha4Ing, "11.50","15"));
+            menuList.add(new MenuDetail(R.drawable.buddha5,R.string.buddha5Name,R.string.buddha5Ing, "11.50","15"));
+        }
+        else if (type.equalsIgnoreCase("Burrito Bowl")){
+            menuList.add(new MenuDetail(R.drawable.burrito1,R.string.burrito1Name,R.string.burrito1Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.burrito2,R.string.burrito2Name,R.string.burrito2Ing, "12.50","15"));
+            menuList.add(new MenuDetail(R.drawable.burrito3,R.string.burrito3Name,R.string.burrito3Ing, "12.00","15"));
+            menuList.add(new MenuDetail(R.drawable.burrito4,R.string.burrito4Name,R.string.burrito4Ing, "12.00","15"));
+            menuList.add(new MenuDetail(R.drawable.burrito5,R.string.burrito5Name,R.string.burrito5Ing, "12.00","15"));
+        }
+        else if(type.equalsIgnoreCase("Smoothie Bowl")){
+            menuList.add(new MenuDetail(R.drawable.smoothie1,R.string.smoothie1Name,R.string.smoothie1Ing, "10.50","15"));
+            menuList.add(new MenuDetail(R.drawable.smoothie2,R.string.smoothie2Name,R.string.smoothie2Ing, "10.50","15"));
+            menuList.add(new MenuDetail(R.drawable.smoothie3,R.string.smoothie3Name,R.string.smoothie3Ing, "10.50","15"));
+            menuList.add(new MenuDetail(R.drawable.smoothie4,R.string.smoothie4Name,R.string.smoothie4Ing, "10.50","15"));
+        }
 
         //set up recycler view
         mRecyclerView = findViewById(R.id.menuItemRecyclerView);
-        menuAdapter = new MenuAdapter(this, pokeList);
+        menuAdapter = new MenuAdapter(this, menuList);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(menuAdapter);
